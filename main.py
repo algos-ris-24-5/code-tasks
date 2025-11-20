583bdea9e484bbb30c2936efdced90810dbe2d0b
T1ray <tester2016go@gmail.com>
2025-11-20 23:08:28 +0500

    Added structure and function comments

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
+def _data_validation_tridiagonal_determinant(matrix: list[list[int]]) -> None:
+    """
+    Внутренняя функция для проверки корректности ввода данных в трехдиагональную матрицу
+    """
+    pass
+
+def _get_reduced_matrix(matrix: list[list[int]], row: int, column: int) -> list[list[int]]:
+    """
+    Функция для вычисления матрицы с вычеркнутым столбцом и строкой
+    :param matrix: целочисленная трехдиагональная квадратная матрица.
+    :param row: Вычеркиваемый ряд из исходной матрицы.
+    :param column: Вычеркиваемый столбец из исходной матрицы.
+
+    :return: Полученная матрица.
+    """
+    pass
+
+def _get_tridiagonal_determinant(matrix: list[list[int]]) -> int:
+    """
+    Внутренняя функция вычисления определителя трехдиагональной целочисленной квадратной матрицы.
+    :param matrix: целочисленная трехдиагональная квадратная матрица.
+
+    :return: значение определителя.
+    """
+    if len(matrix) == 1:
+        return matrix[0][0]
+    
+    det = 0
+
+
 def get_tridiagonal_determinant(matrix: list[list[int]]) -> int:
     """Вычисляет определитель трехдиагональной целочисленной квадратной матрицы.
     :param matrix: целочисленная трехдиагональная квадратная матрица.
 
     :return: значение определителя.
     """
-    pass
+    
+    _data_validation_tridiagonal_determinant(matrix)
+    return _get_tridiagonal_determinant(matrix)
 
 
 def main():
