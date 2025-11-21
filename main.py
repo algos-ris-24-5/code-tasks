7c362ace705c783f06c8893fb7f60d6680e15db7
oatmeal407 <oatmeal.00@mail.ru>
2025-11-21 17:52:21 +0500

    Задание 2 вариант 1 lollygag

--- main.py
+++ main.py
@@ -6,7 +6,41 @@ def calculate_determinant(matrix: list[list[int]]) -> int:
     квадратной матрицей
     :return: значение определителя
     """
-    pass
+    validate_matrix(matrix)
+
+    if len(matrix) == 1:
+        return matrix[0][0]
+    det = 0
+    for idx, item in enumerate(matrix[0]):
+        det += (
+            item
+            * (-1) ** idx
+            * calculate_determinant(get_reduced_matrix(matrix, 0, idx))
+        )
+    return det
+
+
+def get_reduced_matrix(matrix, row_idx, col_idx):
+    n = len(matrix)
+    result = []
+    for i in range(n):
+        if i != row_idx:
+            new_row = []
+            for j in range(n):
+                if j != col_idx:
+                    new_row.append(matrix[i][j])
+            result.append(new_row)
+    return result
+
+
+def validate_matrix(matrix):
+    if not matrix or not isinstance(matrix, list):
+        raise Exception("Матрица пуста или не является списком")
+
+    n = len(matrix)
+    for row in matrix:
+        if len(row) != n:
+            raise Exception("Матрица должна быть квадратной")
 
 
 def main():
