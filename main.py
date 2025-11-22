901678aaf035f5fc91b6899f6da3101c3477abcf
T1ray <tester2016go@gmail.com>
2025-11-22 21:47:10 +0500

    Completed task 2 v1

--- main.py
+++ main.py
@@ -2,9 +2,53 @@ def _data_validation_tridiagonal_determinant(matrix: list[list[int]]) -> None:
     """
     Внутренняя функция для проверки корректности ввода данных в трехдиагональную матрицу
     """
-    pass
+    if not matrix or not isinstance(matrix, list):
+        raise Exception("Incorrect input data type")
 
-def _get_reduced_matrix(matrix: list[list[int]], row: int, column: int) -> list[list[int]]:
+    number_rows = len(matrix)
+    for row in matrix:
+        if len(row) != number_rows:
+            raise Exception("A rectangular matrix or an echelon matrix is ​​transmitted")
+
+    is_incorrect_filled_in = False
+    if number_rows > 2:
+        for idx, el in enumerate(matrix[0]):
+            if idx > 1 and el != 0:
+                is_incorrect_filled_in = True
+                break
+        cur_start_index = 0
+        for row_index in range(1, number_rows - 1):
+            for idx, el in enumerate(matrix[row_index]):
+                if idx not in range(cur_start_index, cur_start_index + 3) and el != 0:
+                    is_incorrect_filled_in = True
+                    break
+            cur_start_index += 1
+        for idx, el in enumerate(matrix[number_rows - 1]):
+            if idx < number_rows - 2 and el != 0:
+                is_incorrect_filled_in = True
+                break
+    if is_incorrect_filled_in:
+        raise Exception("A normal matrix was transmitted, not a tridiagonal matrix")
+
+    is_inconsistent_data = False
+    for i in range(number_rows - 1):
+        if matrix[i][i] != matrix[i + 1][i + 1]:
+            is_inconsistent_data = True
+            break
+    for i in range(number_rows - 2):
+        if (
+            matrix[i + 1][i] != matrix[i + 2][i + 1]
+            or matrix[i][i + 1] != matrix[i + 1][i + 2]
+        ):
+            is_inconsistent_data = True
+            break
+    if is_inconsistent_data:
+        raise Exception("The diagonals contain non-coinciding values")
+
+
+def _get_reduced_matrix(
+    matrix: list[list[int]], row: int, column: int
+) -> list[list[int]]:
     """
     Функция для вычисления матрицы с вычеркнутым столбцом и строкой
     :param matrix: целочисленная трехдиагональная квадратная матрица.
@@ -13,7 +57,21 @@ def _get_reduced_matrix(matrix: list[list[int]], row: int, column: int) -> list[
 
     :return: Полученная матрица.
     """
-    pass
+
+    created_matrix: list[list[int]] = []
+    curi = -1
+    for i in range(len(matrix)):
+        if i == row:
+            continue
+        curi += 1
+        created_matrix.append([])
+        for j in range(len(matrix)):
+            if j == column:
+                continue
+            created_matrix[curi].append(matrix[i][j])
+
+    return created_matrix
+
 
 def _get_tridiagonal_determinant(matrix: list[list[int]]) -> int:
     """
@@ -24,8 +82,14 @@ def _get_tridiagonal_determinant(matrix: list[list[int]]) -> int:
     """
     if len(matrix) == 1:
         return matrix[0][0]
-    
-    det = 0
+    if len(matrix) == 2:
+        return matrix[0][0] ** 2 - (matrix[0][1] * matrix[1][0])
+
+    return matrix[0][0] * _get_tridiagonal_determinant(
+        _get_reduced_matrix(matrix, 0, 0)
+    ) - matrix[1][0] * matrix[0][1] * _get_tridiagonal_determinant(
+        _get_reduced_matrix(_get_reduced_matrix(matrix, 0, 0), 0, 0)
+    )
 
 
 def get_tridiagonal_determinant(matrix: list[list[int]]) -> int:
@@ -34,7 +98,7 @@ def get_tridiagonal_determinant(matrix: list[list[int]]) -> int:
 
     :return: значение определителя.
     """
-    
+
     _data_validation_tridiagonal_determinant(matrix)
     return _get_tridiagonal_determinant(matrix)
 
