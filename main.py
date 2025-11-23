dc21f4cc020aeaa1f9d2b8b2f74c4afa62b626d0
suyaru <kudo4477@gmail.com>
2025-11-23 23:08:25 +0500

    Task 2

--- main.py
+++ main.py
@@ -6,7 +6,56 @@ def calculate_determinant(matrix: list[list[int]]) -> int:
     квадратной матрицей
     :return: значение определителя
     """
-    pass
+    _validate_matrix(matrix)
+    return _determinant_recursive(matrix)
+
+
+def _determinant_recursive(matrix):
+    if len(matrix) == 1:
+        return matrix[0][0]
+    det = 0
+    for idx, item in enumerate(matrix[0]):
+        det += (
+            item
+            * (-1) ** idx 
+            * _determinant_recursive(_get_reduced_matrix(matrix, 0, idx))
+        )
+    return det
+
+
+def _get_reduced_matrix(matrix, row_idx, col_idx):
+    reduced = []
+    for i in range(len(matrix)):
+        if i == row_idx:
+            continue
+        new_row = []
+        for j in range(len(matrix[i])):
+            if j == col_idx:
+                continue
+            new_row.append(matrix[i][j])
+        reduced.append(new_row)
+    return reduced
+
+
+def _validate_matrix(matrix):
+    if matrix is None:
+        raise Exception("Matrix is None")
+
+    if not isinstance(matrix, list):
+        raise Exception("Matrix is not a list")
+
+    n = len(matrix)
+    if n == 0:
+        raise Exception("Matrix is empty")
+
+    for i, row in enumerate(matrix):
+        if not isinstance(row, list):
+            raise Exception(f"Row {i} is not a list")
+        if len(row) != n:
+            raise Exception("Matrix is not square")
+        for j, elem in enumerate(row):
+            if not isinstance(elem, int):
+                raise Exception(f"Element at ({i}, {j}) is not an integer")
 
 
 def main():
