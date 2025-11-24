6640367bb47f2d0670738b9cb1e69cb556fc9e94
MrMefedron <gleb70706@gmail.com>
2025-11-24 13:49:38 +0500

    Co-authored-by: eapirogov <eapirogov@users.noreply.github.com> Co-authored-by: 1slandec <1slandec@users.noreply.github.com>

--- main.py
+++ main.py
@@ -6,8 +6,29 @@ def calculate_determinant(matrix: list[list[int]]) -> int:
     квадратной матрицей
     :return: значение определителя
     """
-    pass
+    if not matrix or not isinstance(matrix,list):
+        raise Exception("matrix is empty")
+    
+    for i in matrix:
+        if len(i)!=len(matrix):
+            raise Exception("matrix is not square")
+            
+    if len(matrix)==1:
+        return matrix[0][0]
+    
+    det = 0
+    
+    for idx,item in enumerate(matrix[0]):
+        reduced_matrix = _get_reduced_matrix(matrix,0,idx)
+        det+= item * (-1) ** idx * calculate_determinant(reduced_matrix)
+    return det
 
+def _get_reduced_matrix(matrix, zero, idx):
+    new_matrix = [row[:] for row in matrix]
+    new_matrix.pop(zero)
+    for i in range(len(new_matrix)):
+        new_matrix[i].pop(idx)
+    return new_matrix
 
 def main():
     matrix = [[1, 2], [3, 4]]
