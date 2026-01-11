c34800f2ca6ddba0ebd10a5a8890824a5f6ca2f3
Dima-lab-ux <dmi.mash@yandex.ru>
2026-01-11 23:56:40 +0500

    Update dynamic_solver.py

--- problems/knapsack_problem/dynamic_solver.py
+++ problems/knapsack_problem/dynamic_solver.py
@@ -4,43 +4,43 @@ from problems.knapsack_problem.knapsack_abs_solver import (
 )
 from problems.knapsack_problem.errors.error_message_enum import ErrorMessageEnum
 
+
 class DynamicSolver(KnapsackAbstractSolver):
     def get_knapsack(self):
-        """
-        Решает задачу о рюкзаке с использованием метода динамического программирования.
-
-        :return: максимально возможная общая стоимость и список индексов выбранных предметов
-        :rtype: KnapsackSolution
-        """
         for weight in self.weights:
-            if not float(weight).is_integer():
+            if not isinstance(weight, (int, float)):
                 raise ValueError(ErrorMessageEnum.FLOAT_WEIGHT)
-        
+            if isinstance(weight, float) and not weight.is_integer():
+                raise ValueError(ErrorMessageEnum.FLOAT_WEIGHT)
+
         n = self.item_cnt
         W = self.weight_limit
-        
+        weights_int = [int(w) for w in self.weights]
+        costs = self.costs
+
         dp = [[0] * (W + 1) for _ in range(n + 1)]
-        
+
         for i in range(1, n + 1):
-            current_weight = self.weights[i-1]
-            current_cost = self.costs[i-1]
-            
+            current_weight = weights_int[i - 1]
+            current_cost = costs[i - 1]
             for w in range(W + 1):
                 if current_weight > w:
-                    dp[i][w] = dp[i-1][w]
+                    dp[i][w] = dp[i - 1][w]
                 else:
                     dp[i][w] = max(
-                        dp[i-1][w],  # не берем предмет
-                        dp[i-1][w - current_weight] + current_cost  # берем предмет
+                        dp[i - 1][w],
+                        dp[i - 1][w - current_weight] + current_cost
                     )
+
         selected_items = []
         current_weight = W
         for i in range(n, 0, -1):
-            if dp[i][current_weight] != dp[i-1][current_weight]:
-                selected_items.append(i-1)
-                current_weight -= self.weights[i-1]
+            if dp[i][current_weight] != dp[i - 1][current_weight]:
+                selected_items.append(i - 1)
+                current_weight -= weights_int[i - 1]
         selected_items.reverse()
-        return self.KnapsackSolution(dp[n][W], selected_items)
+
+        return KnapsackSolution(dp[n][W], selected_items)
 
 if __name__ == "__main__":
     weights = [11, 4, 8, 6, 3, 5, 5]
