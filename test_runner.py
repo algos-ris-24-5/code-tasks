318d6072ba59995244ce544614b7b8f351e5d24a
MikhailovAlexander <AVMikhailov@hse.ru>
2026-01-11 20:29:32 +0300

    add task 7

---
+++ test_runner.py
@@ -0,0 +1,16 @@
+from unittest import TestLoader, TestSuite, TextTestRunner
+
+from tests.data_structures.lists.test_doubly_linked_list import TestDoublyLinkedList
+
+
+def suite():
+    """Создает набор тест-кейсов для тестирования."""
+    test_suite = TestSuite()
+    test_suite.addTest(TestLoader().loadTestsFromTestCase(TestDoublyLinkedList))
+
+    return test_suite
+
+
+if __name__ == "__main__":
+    runner = TextTestRunner(verbosity=2)
+    runner.run(suite())
