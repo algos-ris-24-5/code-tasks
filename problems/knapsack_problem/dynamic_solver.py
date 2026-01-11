34a11e7b55c32d93aa37fca2484b992f15ba7e3e
Dima-lab-ux <dmi.mash@yandex.ru>
2026-01-11 23:45:42 +0500

    Update dynamic_solver.py

--- problems/knapsack_problem/dynamic_solver.py
+++ problems/knapsack_problem/dynamic_solver.py
@@ -5,15 +5,42 @@ from problems.knapsack_problem.knapsack_abs_solver import (
 
 
 class DynamicSolver(KnapsackAbstractSolver):
-    def get_knapsack(self) -> KnapsackSolution:
+    def get_knapsack(self):
         """
         Решает задачу о рюкзаке с использованием метода динамического программирования.
 
         :return: максимально возможная общая стоимость и список индексов выбранных предметов
         :rtype: KnapsackSolution
         """
-        pass
-
+        for weight in self.weights:
+            if not float(weight).is_integer():
+                raise ValueError(ErrorMessageEnum.FLOAT_WEIGHT)
+        
+        n = self.item_cnt
+        W = self.weight_limit
+        
+        dp = [[0] * (W + 1) for _ in range(n + 1)]
+        
+        for i in range(1, n + 1):
+            current_weight = self.weights[i-1]
+            current_cost = self.costs[i-1]
+            
+            for w in range(W + 1):
+                if current_weight > w:
+                    dp[i][w] = dp[i-1][w]
+                else:
+                    dp[i][w] = max(
+                        dp[i-1][w],  # не берем предмет
+                        dp[i-1][w - current_weight] + current_cost  # берем предмет
+                    )
+        selected_items = []
+        current_weight = W
+        for i in range(n, 0, -1):
+            if dp[i][current_weight] != dp[i-1][current_weight]:
+                selected_items.append(i-1)
+                current_weight -= self.weights[i-1]
+        selected_items.reverse()
+        return self.KnapsackSolution(dp[n][W], selected_items)
 
 if __name__ == "__main__":
     weights = [11, 4, 8, 6, 3, 5, 5]
