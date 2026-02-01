a8aa3d0f4f7df3dc5af394f3646d61130a85e809
MrMefedron <gleb70706@gmail.com>
2026-02-01 18:27:24 +0500

    fix after review

--- matching/max_matching.py
+++ matching/max_matching.py
@@ -3,6 +3,37 @@ from matching.bipartite_graph_matching import BipartiteGraphMatching
 from matching.errors.error_message_enum import ErrorMessageEnum
 from collections import deque
 
+def get_tree(bipartite_graph, matching, order):
+    levels = [-1] * order
+    queue = deque()
+    for current_left in range(order):
+        if not matching.is_left_covered(current_left):
+            levels[current_left] = 0
+            queue.append(current_left)
+    found = False
+    while queue:
+        current_left = queue.popleft()
+        for neighbor_of_left in bipartite_graph.right_neighbors(current_left):
+            matched_left = matching.get_left_match(neighbor_of_left)
+            if matched_left != -1 and levels[matched_left] == -1:
+                levels[matched_left] = levels[current_left] + 1
+                queue.append(matched_left)
+            elif matched_left == -1:
+                found = True
+    return levels, found
+
+def find_chain(bipartite_graph, matching, current_left, levels, visited):
+    visited[current_left] = True
+    for neighbor_of_left in bipartite_graph.right_neighbors(current_left):
+        matched_left = matching.get_left_match(neighbor_of_left)
+        if matched_left == -1:
+            return [(current_left, neighbor_of_left)]
+        elif not visited[matched_left] and levels[matched_left] == levels[current_left] + 1:
+            path = find_chain(bipartite_graph, matching, matched_left, levels, visited)
+            if path is not None:
+                path.append((current_left, neighbor_of_left))
+                return path
+    return None
 
 def get_max_matching(bipartite_graph: BipartiteGraph) -> BipartiteGraphMatching:
     """
@@ -23,47 +54,15 @@ def get_max_matching(bipartite_graph: BipartiteGraph) -> BipartiteGraphMatching:
     
     order = bipartite_graph.order
     matching = BipartiteGraphMatching(order)
-    
-    def get_tree():
-        levels = [-1] * order
-        queue = deque()
-        for current_left in range(order):
-            if not matching.is_left_covered(current_left):
-                levels[current_left] = 0
-                queue.append(current_left)
-        found = False
-        while queue:
-            current_left = queue.popleft()
-            for neighbor_of_left in bipartite_graph.right_neighbors(current_left):
-                matched_left = matching.get_left_match(neighbor_of_left)
-                if matched_left != -1 and levels[matched_left] == -1:
-                    levels[matched_left] = levels[current_left] + 1
-                    queue.append(matched_left)
-                elif matched_left == -1:
-                    found = True
-        return levels, found
-
-    def find_chain(current_left, levels, visited):
-        visited[current_left] = True
-        for neighbor_of_left in bipartite_graph.right_neighbors(current_left):
-            matched_left = matching.get_left_match(neighbor_of_left)
-            if matched_left == -1:
-                return [(current_left, neighbor_of_left)]
-            elif not visited[matched_left] and levels[matched_left] == levels[current_left] + 1:
-                path = find_chain(matched_left, levels, visited)
-                if path is not None:
-                    path.append((current_left, neighbor_of_left))
-                    return path
-        return None
 
     while True:
-        levels, found = get_tree()
+        levels, found = get_tree(bipartite_graph, matching, order)
         if not found:
             break
         for current_left in range(order):
             if not matching.is_left_covered(current_left):
                 visited = [False] * order
-                path = find_chain(current_left, levels, visited)
+                path = find_chain(bipartite_graph, matching, current_left, levels, visited)
                 if path:
                     for left, right in reversed(path):
                         matched_left = matching.get_left_match(right)
