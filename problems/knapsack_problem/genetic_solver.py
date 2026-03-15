ea00e765a1fdb5ce90a4a6210b358849d58269db
Маргарита Дудырева <dudirevamar@outlook.com>
2026-03-15 21:59:20 +0500

    done

--- problems/knapsack_problem/genetic_solver.py
+++ problems/knapsack_problem/genetic_solver.py
@@ -59,15 +59,49 @@ class GeneticSolver(KnapsackAbstractSolver):
             solver = BruteForceSolver(self.weights, self.costs, self.weight_limit)
             return solver.get_knapsack()
         
-        for _ in range(epoch_cnt):
-            items_list = sorted(self.__population.items(), key=lambda x: x[1], reverse=True)
+        best_fitness = -1
+        best_key = None
+        stagnation_counter = 0
+        stagnation_limit = max(10, epoch_cnt // 10)
+        
+        for epoch in range(epoch_cnt):
+            # Обновляем фитнес для всех особей
+            items_list = []
+            for key in list(self.__population.keys()):
+                self.__population[key] = self.__get_fit(key)
+                items_list.append((key, self.__population[key]))
             
+            # Сортируем по фитнесу
+            items_list.sort(key=lambda x: x[1], reverse=True)
+            
+            # Проверяем улучшение
+            current_best = items_list[0][1]
+            if current_best > best_fitness:
+                best_fitness = current_best
+                best_key = items_list[0][0]
+                stagnation_counter = 0
+            else:
+                stagnation_counter += 1
+            
+            # Ранняя остановка, если нет улучшений
+            if stagnation_counter >= stagnation_limit:
+                break
+            
+            # Элитизм - сохраняем 10% лучших
             elite_count = max(1, self.__population_cnt // 10)
-            new_population = dict(items_list[:elite_count])
+            new_population = {}
+            for i in range(elite_count):
+                key, fitness = items_list[i]
+                new_population[key] = fitness
             
-            strongest = [key for key, _ in items_list[:self.__population_cnt // 2]]
+            # Сильнейшие для скрещивания (50% лучших)
+            strongest = [key for key, _ in items_list[:max(2, self.__population_cnt // 2)]]
             
-            while len(new_population) < self.__population_cnt:
+            # Заполняем остальную часть популяции
+            attempts = 0
+            max_attempts = self.__population_cnt * 5
+            
+            while len(new_population) < self.__population_cnt and attempts < max_attempts:
                 parent1 = rnd.choice(strongest)
                 parent2 = rnd.choice(strongest)
                 
@@ -76,16 +110,33 @@ class GeneticSolver(KnapsackAbstractSolver):
                 child1 = self.__mutation(child1)
                 child2 = self.__mutation(child2)
                 
-                for child in (child1, child2):
-                    if len(new_population) < self.__population_cnt and child not in new_population:
-                        fitness = self.__get_fit(child)
-                        if fitness > 0:
-                            new_population[child] = fitness
+                if child1 not in new_population:
+                    fitness1 = self.__get_fit(child1)
+                    if fitness1 > 0:
+                        new_population[child1] = fitness1
+                
+                if len(new_population) < self.__population_cnt and child2 not in new_population:
+                    fitness2 = self.__get_fit(child2)
+                    if fitness2 > 0:
+                        new_population[child2] = fitness2
+                
+                attempts += 1
+            
+            # Если не удалось заполнить, добавляем лучших из предыдущей популяции
+            if len(new_population) < self.__population_cnt:
+                for key, fitness in items_list:
+                    if key not in new_population:
+                        new_population[key] = fitness
+                        if len(new_population) >= self.__population_cnt:
+                            break
             
             self.__population = new_population
         
-        best_key = max(self.__population.items(), key=lambda x: x[1])[0]
-        best_fitness = self.__population[best_key]
+        # Если не нашли лучшего, ищем сейчас
+        if best_key is None:
+            best_key = max(self.__population.items(), key=lambda x: x[1])[0]
+            best_fitness = self.__population[best_key]
+        
         best_binary = self.__mask.format(best_key)
         best_items = [i for i, bit in enumerate(best_binary) if bit == '1']
         
@@ -97,36 +148,51 @@ class GeneticSolver(KnapsackAbstractSolver):
         max_attempts = population_cnt * 5
         
         while len(population) < population_cnt and attempts < max_attempts:
-            item = rnd.randint(0, 2**self.item_cnt - 1)
+            item = 0
+            # С вероятностью 30% включаем каждый предмет
+            for i in range(self.item_cnt):
+                if rnd.random() < 0.3:
+                    item |= (1 << i)
+            
             fitness = self.__get_fit(item)
             if fitness > 0 and item not in population:
                 population[item] = fitness
             attempts += 1
         
         if len(population) == 0:
-            population[0] = 0
+            population[0] = self.__get_fit(0)
         
         return population
 
     def __cross_items(self, ancestor1: int, ancestor2: int) -> tuple[int, int]:
+        """Равномерное скрещивание."""
         child1 = 0
         child2 = 0
-        mask = rnd.getrandbits(self.item_cnt)
         
-        child1 = (ancestor1 & mask) | (ancestor2 & ~mask)
-        child2 = (ancestor2 & mask) | (ancestor1 & ~mask)
+        for i in range(self.item_cnt):
+            if rnd.random() < 0.5:
+                if ancestor1 & (1 << i):
+                    child1 |= (1 << i)
+                if ancestor2 & (1 << i):
+                    child2 |= (1 << i)
+            else:
+                if ancestor2 & (1 << i):
+                    child1 |= (1 << i)
+                if ancestor1 & (1 << i):
+                    child2 |= (1 << i)
         
         return child1, child2
 
     def __mutation(self, item_set: int) -> int:
-        mutation_rate = 0.02
-        mask = 0
+        """Мутация особи с вероятностью 5%."""
+        mutation_rate = 0.05
         for i in range(self.item_cnt):
             if rnd.random() < mutation_rate:
-                mask |= (1 << i)
-        return item_set ^ mask
+                item_set ^= (1 << i)
+        return item_set
 
     def __get_fit(self, item: int) -> int:
+        """Вычисляет значение фитнес-функции для особи."""
         binary = self.__mask.format(item)
         selected = [c == '1' for c in binary]
         return self.get_cost(selected)
