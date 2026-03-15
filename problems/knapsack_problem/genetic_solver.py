058255cddbacfc89acb4eb71305a5eb1eed20531
Dima-lab-ux <dmi.mash@yandex.ru>
2026-03-15 21:24:29 +0500

    Update genetic_solver.py

--- problems/knapsack_problem/genetic_solver.py
+++ problems/knapsack_problem/genetic_solver.py
@@ -40,7 +40,8 @@ class GeneticSolver(KnapsackAbstractSolver):
         """
         super().__init__(weights, costs, weight_limit)
         self.__mask = "{0:0" + str(len(weights)) + "b}"
-        self.__population_cnt = min(2**self.item_cnt / 2, POPULATION_LIMIT)
+        self.__population_cnt = min(2**self.item_cnt // 2, POPULATION_LIMIT)
+        self.__population_cnt = max(self.__population_cnt, 10) 
         self.__population = self.__generate_population(self.__population_cnt)
 
     @property
@@ -53,21 +54,121 @@ class GeneticSolver(KnapsackAbstractSolver):
             population_data.append((self.__mask.format(key), self.__population[key]))
         return population_data
 
-    def get_knapsack(self, epoch_cnt=EPOCH_CNT) -> KnapsackSolution:
+    def get_knapsack(self, epoch_cnt=EPOCH_CNT, stagnation_limit=20) -> KnapsackSolution:
         """Решает задачу о рюкзаке с использованием генетического алгоритма."""
-        pass
 
-    def __generate_population(self, population_cnt: int) -> dict[int:int]:
-        pass
+        try:
+            if self.item_cnt <= BRUTE_FORCE_BOUND:
+                brute_solver = BruteForceSolver(self._weights, self._costs, self._weight_limit)
+                return brute_solver.get_knapsack()
+
+            best_solution = None
+            best_cost = 0
+            stagnation_counter = 0
+
+            for epoch in range(epoch_cnt):
+                for item_set in list(self.__population.keys()):
+                    self.__population[item_set] = self.__get_fit(item_set)
+
+                if self.__population:
+                    current_leader = max(self.__population, key=self.__population.get)
+                    current_fitness = self.__population[current_leader]
+
+                    if current_fitness > best_cost:
+                        best_cost = current_fitness
+                        best_solution = current_leader
+                        stagnation_counter = 0
+                    else:
+                        stagnation_counter += 1
+
+                    if stagnation_counter >= stagnation_limit:
+                        break
+
+                new_population = {}
+
+                sorted_pop = sorted(self.__population.items(), key=lambda x: x[1], reverse=True)
+                elite_count = max(1, len(sorted_pop) // 10)
+                for i in range(min(elite_count, len(sorted_pop))):
+                    new_population[sorted_pop[i][0]] = sorted_pop[i][1]
+
+                while len(new_population) < self.__population_cnt:
+                    ancestor1 = self.__select_parent()
+                    ancestor2 = self.__select_parent()
+
+                    child1, child2 = self.__cross_items(ancestor1, ancestor2)
+
+                    child1 = self.__mutation(child1)
+                    child2 = self.__mutation(child2)
+
+                    if len(new_population) < self.__population_cnt:
+                        new_population[child1] = self.__get_fit(child1)
+                    if len(new_population) < self.__population_cnt:
+                        new_population[child2] = self.__get_fit(child2)
+
+                self.__population = new_population
+
+            if best_solution is None and self.__population:
+                best_solution = max(self.__population, key=self.__population.get)
+
+            if best_solution is not None:
+                mask_str = self.__mask.format(best_solution)
+                items = [idx for idx, bit in enumerate(mask_str) if bit == '1']
+                cost = self.get_cost([bit == '1' for bit in mask_str])
+                return KnapsackSolution(cost=cost, items=items)
+            else:
+                return KnapsackSolution(cost=0, items=[])
+        except Exception as e:
+            print(f"Error in get_knapsack: {e}")
+            return KnapsackSolution(cost=0, items=[])
+
+    def __generate_population(self, population_cnt: int) -> dict[int, int]:
+        population = {}
+        max_val = 2**self.item_cnt
+
+        for _ in range(population_cnt):
+            item_set = rnd.randint(0, max_val - 1)
+            population[item_set] = self.__get_fit(item_set)
+
+        return population
 
     def __cross_items(self, ancestor1: int, ancestor2: int) -> tuple[int, int]:
-        pass
+        if self.item_cnt <= 1:
+            return ancestor1, ancestor2
+
+        cross_point = rnd.randint(1, self.item_cnt - 1)
+        mask = (1 << self.item_cnt) - 1
+
+        a1_lower = ancestor1 & ((1 << cross_point) - 1)
+        a1_upper = ancestor1 >> cross_point
+        a2_lower = ancestor2 & ((1 << cross_point) - 1)
+        a2_upper = ancestor2 >> cross_point
+
+        child1 = ((a1_upper << cross_point) | a2_lower) & mask
+        child2 = ((a2_upper << cross_point) | a1_lower) & mask
+
+        return child1, child2
 
     def __mutation(self, item_set: int) -> int:
-        pass
+        mutation_rate = 0.1
 
-    def __get_fit(self, item):
-        pass
+        for i in range(self.item_cnt):
+            if rnd.random() < mutation_rate:
+                item_set ^= (1 << i)
+
+        return item_set
+
+    def __get_fit(self, item: int) -> int:
+        mask_str = self.__mask.format(item)
+        selected = [bit == '1' for bit in mask_str]
+        return self.get_cost(selected)
+
+    def __select_parent(self) -> int:
+        tournament_size = 3
+        candidates = rnd.sample(
+            list(self.__population.keys()),
+            min(tournament_size, len(self.__population))
+        )
+        return max(candidates, key=lambda x: self.__population[x])
 
 
 if __name__ == "__main__":
